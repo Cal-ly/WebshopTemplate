c50bd55a164e5052b87e5fe7d5c68b52c2752853
Cal-ly <cal-ly@outlook.dk>
2024-02-14 20:34:12 +0100

    Update

--- WebshopTemplate/WebshopTemplate/Program.cs
+++ WebshopTemplate/WebshopTemplate/Program.cs
@@ -35,9 +35,7 @@ var connectionString = builder.Configuration.GetConnectionString("ApplicationDbC
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddIdentity<Staff, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddIdentityCore<Staff>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
